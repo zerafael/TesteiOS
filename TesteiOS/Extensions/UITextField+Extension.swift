e726d3590c6a62d6a093c54d5ccced4c66e86a5e
José Rafael <zerafael1@gmail.com>
2019-12-11 00:19:53 -0400

    Adicionado funções ao UIColor para simplificar o uso da classe passando uma string rgb e retornando uma cor

---
+++ TesteiOS/Extensions/UITextField+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UITextField+Extension.swift
+//  TesteiOS
+//
+//  Created by José Rafael Ferraz Pacheco on 10/12/19.
+//  Copyright © 2019 José Rafael Ferraz Pacheco. All rights reserved.
+//
+
+import Foundation
