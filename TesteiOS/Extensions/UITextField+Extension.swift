db749e478e0d98da4a7afbdd0ff1def551106ca6
José Rafael <zerafael1@gmail.com>
2019-12-11 00:20:58 -0400

    Adicionado funções para gerar um padding no UITextField

--- TesteiOS/Extensions/UITextField+Extension.swift
+++ TesteiOS/Extensions/UITextField+Extension.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITextField {
+    
+    func setLeftPadding(_ value: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: value, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPadding(_ value: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: value, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
