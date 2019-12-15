5b740506b8fe3d16aafe583ddee2c361bdddbc7d
José Rafael <zerafael1@gmail.com>
2019-12-15 11:41:26 -0400

    Adiciona modelo Statement

---
+++ TesteiOS/Models/Statement.swift
@@ -0,0 +1,9 @@
+//
+//  Statements.swift
+//  TesteiOS
+//
+//  Created by José Rafael Ferraz Pacheco on 15/12/19.
+//  Copyright © 2019 José Rafael Ferraz Pacheco. All rights reserved.
+//
+
+import Foundation
