68a6274e087a87cfcfc5b7253b3bd7d59894f917
José Rafael <zerafael1@gmail.com>
2019-12-15 11:41:59 -0400

    .

--- TesteiOS/Models/Statement.swift
+++ TesteiOS/Models/Statement.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct Statement {
+    var title: String
+    var desc: String
+    var date: String
+    var value: Float
+}
+
+extension Statement {
+    init?(json: [String: AnyObject]) {
+        title = json["title"] as! String
+        desc = json["desc"] as! String
+        value = Float(truncating: json["value"] as! NSNumber)
+        date = ""
+        date = formatDate(date: json["date"] as! String)
+    }
+    
+    mutating func formatDate(date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let tmpDate = dateFormatter.date(from: date)
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.string(from: tmpDate!)
+    }
+}
